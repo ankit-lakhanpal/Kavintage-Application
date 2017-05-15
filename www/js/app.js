fa99188dccc815cbc5d51ebf9ba23f7a14f2ff7e
Ankit Lakhanpal <er.ankit2702@gmail.com>
2017-05-16 02:32:28 +0530

    Animation working

--- www/js/app.js
+++ www/js/app.js
@@ -73,78 +73,87 @@ var example = angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-/*
-  This directive is used to disable the "drag to open" functionality of the Side-Menu
-  when you are dragging a Slider component.
-*/
-.directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function($ionicSideMenuDelegate, $rootScope) {
+  /*
+    This directive is used to disable the "drag to open" functionality of the Side-Menu
+    when you are dragging a Slider component.
+  */
+  .directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function($ionicSideMenuDelegate, $rootScope) {
     return {
-        restrict: "A",
-        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+      restrict: "A",
+      controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
 
-            function stopDrag(){
-              $ionicSideMenuDelegate.canDragContent(false);
-            }
+        function stopDrag() {
+          $ionicSideMenuDelegate.canDragContent(false);
+        }
 
-            function allowDrag(){
-              $ionicSideMenuDelegate.canDragContent(true);
-            }
+        function allowDrag() {
+          $ionicSideMenuDelegate.canDragContent(true);
+        }
 
-            $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
-            $element.on('touchstart', stopDrag);
-            $element.on('touchend', allowDrag);
-            $element.on('mousedown', stopDrag);
-            $element.on('mouseup', allowDrag);
+        $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
+        $element.on('touchstart', stopDrag);
+        $element.on('touchend', allowDrag);
+        $element.on('mousedown', stopDrag);
+        $element.on('mouseup', allowDrag);
 
-        }]
+      }]
     };
-}])
+  }])
 
-/*
-  This directive is used to open regular and dynamic href links inside of inappbrowser.
-*/
-.directive('hrefInappbrowser', function() {
-  return {
-    restrict: 'A',
-    replace: false,
-    transclude: false,
-    link: function(scope, element, attrs) {
-      var href = attrs['hrefInappbrowser'];
+  /*
+    This directive is used to open regular and dynamic href links inside of inappbrowser.
+  */
+  .directive('hrefInappbrowser', function() {
+    return {
+      restrict: 'A',
+      replace: false,
+      transclude: false,
+      link: function(scope, element, attrs) {
+        var href = attrs['hrefInappbrowser'];
 
-      attrs.$observe('hrefInappbrowser', function(val){
-        href = val;
-      });
+        attrs.$observe('hrefInappbrowser', function(val) {
+          href = val;
+        });
 
-      element.bind('click', function (event) {
+        element.bind('click', function(event) {
 
-        window.open(href, '_system', 'location=yes');
+          window.open(href, '_system', 'location=yes');
 
-        event.preventDefault();
-        event.stopPropagation();
+          event.preventDefault();
+          event.stopPropagation();
 
-      });
-    }
-  };
-});
+        });
+      }
+    };
+  });
 
 example.controller("ExampleController", function($scope, $ionicSlideBoxDelegate) {
   $scope.navSlide = function(index) {
     $ionicSlideBoxDelegate.slide(index, 500);
-  }
+  };
+
+  $scope.animateVisuals = function() {
+    var buttons = document.getElementById('thickShakes-list-item13');
+    move(buttons)
+      .ease('in-out')
+      .x(-200)
+      .duration('0.5s')
+      .end();
+  };
 });
 
 example.config(function($ionicConfigProvider) {
@@ -158,4 +167,8 @@ example.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
   }
 });
 
-var animation=angular.module('app', ['ng-fx', 'angular-animate']);
+// var animation=angular.module('app', ['ng-fx', 'angular-animate']);
+
+example.controller('AppCtrler', function($scope) {
+
+});
