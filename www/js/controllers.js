4bfbc954c655fd247fca8409a66f88641e152b4b
Ankit Lakhanpal <er.ankit2702@gmail.com>
2017-06-03 01:18:08 +0530

    updated 28 may

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -35,6 +35,7 @@ var example = angular.module('starter.controllers', [])
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
+
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
